f427868ff00c123e5638a46c583a0fffeed1349e
HumphreyJ <joshhumphrey1@gmail.com>
2020-07-22 13:18:25 -0500

    testing steam API getting a sweet cors error.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import Header from './header/Header';
 import Inputs from './inputs/Inputs';
+import axios from 'axios';
 
 const Main = () => {
+    useEffect(() => {
+        axios
+        .get(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM_KEY}&steamids=${process.env.MY_STEAM_ID}`)
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    })
     return (
         <div>
             <Header />
